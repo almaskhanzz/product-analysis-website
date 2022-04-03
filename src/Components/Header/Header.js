d3295575fb27a79236fd67286ff61102ecffe802
Almas Khan <almas.rbm@gmail.com>
2022-04-04 03:40:57 +0600

    navigation bar updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,12 +3,14 @@ import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 const Header = () => {
     return (
-        <div>
-            <CustomLink to='/home'>HOME</CustomLink>
-            <CustomLink to='/reviews'>REVIEWS</CustomLink>
-            <CustomLink to='/dashboard'>DASHBOARD</CustomLink>
-            <CustomLink to='/blogs'>BLOGS</CustomLink>
-            <CustomLink to='/about'>ABOUT</CustomLink>
+        <div className='header-container'>
+            <nav>
+                <CustomLink to='/home'>HOME</CustomLink>
+                <CustomLink to='/reviews'>REVIEWS</CustomLink>
+                <CustomLink to='/dashboard'>DASHBOARD</CustomLink>
+                <CustomLink to='/blogs'>BLOGS</CustomLink>
+                <CustomLink to='/about'>ABOUT</CustomLink>
+            </nav>
         </div>
     );
 };
