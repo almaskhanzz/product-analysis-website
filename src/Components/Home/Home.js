6aeee7dfc958d9e341092e105ad7153fdcebe143
Almas Khan <almas.rbm@gmail.com>
2022-04-04 15:43:02 +0600

    home's heading section updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './Home.css';
+import Airpods from '../../Assets/Images/airpods-pro_1.jpg';
 const Home = () => {
     return (
-        <div>
-            <h1>Hello from Home</h1>
+        <div className='home-container'>
+            <div className='home-preview'>
+                <div className='heading'>
+                    <h1>A magical connection to your devices</h1>
+                    <h3>Spatial audio with
+                        dynamic head tracking</h3>
+                    <p>AirPods Pro are the only in-ear headphones with Active Noise Cancellation that continuously adapts to the geometry of your ear and the fit of the ear tips — blocking out the world so you can focus on what you're listening to.</p>
+                    <button>Live Demo</button>
+                </div>
+                <div className='image-container'>
+                    <img src={Airpods} alt="" />
+                </div>
+            </div>
+            <div className='review-container'>
+                <h2>Our Customer Reviews</h2>
+            </div>
         </div>
     );
 };
